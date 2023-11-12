beab896a5c7d7df0d10616696da865dd1cc5b894
Riswick <riswickelvariste@gmail.com>
2023-11-11 20:43:33 -0400

    loop with js

---
+++ sript.js
@@ -0,0 +1,48 @@
+//loop with for
+//const pay = ["Haiti", "brasil", "peru", "france", "canada", "Usa"];
+//function loop() {
+//debugger;
+//for (let i = 0; i < pay.length; i++) {
+//console.log(pay[i]);
+//}
+//}
+
+//loop();
+
+//loop with for of
+//const pays = ["Haiti", "brasil", "peru", "france", "canada", "Usa"];
+//function loops() {
+//for (pay of pays) {
+//console.log(pays);
+//}
+//}
+//loops();
+//loop with foreach
+
+//const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//number.forEach((numbers) => {
+// console.log(number);
+//});
+
+//loop with while
+
+//let loading = 0;
+
+//while (loading < 100) {
+//console.log("websit is still loading");
+
+//loading++;
+//}
+
+//loop with Map
+
+//const number = [2, 6, 4, 8, 23, 54];
+
+//const numberDouble = number.map(double);
+
+//function double(value, index, arr) {
+// return value * 2;
+//}
+
+//console.log(numberDouble);
